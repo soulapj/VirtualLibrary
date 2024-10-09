96bc35d6db6eba6c3b5e77dbe83f16959ba2768e
balamzor <balam.lml@gmail.com>
2024-10-09 15:13:51 +0200

    intial branch commit + setup HomeView.vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,61 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <table>
+      <tr>
+        <th>Title</th>
+        <th>Author</th>
+        <th>ISBN</th>
+        <th>Actions</th>
+      </tr>
+      <tr>
+        <td>Titre du livre</td>
+        <td>Auteur du livre</td>
+        <td>ISBN du livre</td>
+        <td>
+          <button>Get Info</button>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  components: {},
+  methods: {
+    getBookInfo(book) {
+      this.$store.dispatch('getInfo', book.id);
+    },
+    getBookList() {
+      this.$store.dispatch('getBooks');
+    },
+  },
+  computed: {
+    books() {
+      return this.$store.state.books;
+    },
+  },
+  mounted() {
+    this.getBookList();
+  },
+};
 </script>
+
+<style scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+table {
+  border: 2px solid black;
+}
+th {
+  border: 2px solid black;
+}
+td {
+  border: 2px solid black;
+}
+</style>
