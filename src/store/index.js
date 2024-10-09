1bc47b790650cea31b5007dc7602cd754d0367a1
balamzor <balam.lml@gmail.com>
2024-10-09 16:37:29 +0200

    passage en LF

--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,34 @@
-import { createStore } from 'vuex'
-import axios from "axios";
+import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
     listeBook: [],
     addBook: {},
-    document: []
+    document: [],
+    test: [],
   },
   mutations: {
-    removeBook(state, id){
-      state.listeBook = state.listeBook.filter((book) => id !== book.id)
+    removeBook(state, id) {
+      state.listeBook = state.listeBook.filter((book) => id !== book.id);
     },
-    setDocument(state, val){
+    setDocument(state, val) {
       state.document = val;
-    }
-
+    },
   },
   actions: {
-    modifierLivre(context, livre){
-      let livreModif = [...context.state.   , livre];
+    modifierLivre(context, livre) {
+      let livreModif = [...context.state.test, livre];
     },
-    appelApi({ commit }, query = null){
-      let url = "https://openlibrary.org/api/search.json";
+    appelApi({ commit }, query = null) {
+      let url = 'https://openlibrary.org/api/search.json';
       if (query) {
         url += `${query}`;
       }
       axios.get(url).then((res) => {
-        commit("setDocument", res.data.docs)
-      })
-    } 
+        commit('setDocument', res.data.docs);
+      });
+    },
   },
-  getters: {
-  },
-})
+  getters: {},
+});
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,7 +37,7 @@ export default {
     },
   },
   mounted() {
-    this.getBookList();
+    this.$store.dispatch('appelApi');
   },
 };
 </script>
