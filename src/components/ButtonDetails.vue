169b5ca68b2e604054e8b3067648864330cfcbe5
Victor <victordevos.pro@gmail.com>
2024-10-09 15:15:32 +0200

    add button component and update src file

---
+++ src/components/ButtonDetails.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <button @click="emitDetails">{{ label }}</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            label: {
+                type: String,
+                default: 'Show details'
+            },
+            color: {
+                type: String,
+                default: 'blue'
+            }
+        },
+        methods:{
+            emitDetails(){
+                this.$emit('show-details');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+
+button {
+    background-color: v-bind(color);
+    border: none;
+    border-radius: 8px;
+    cursor: pointer;
+}
+
+</style>
