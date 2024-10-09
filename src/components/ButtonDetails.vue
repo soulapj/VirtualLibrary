9e1daf7587ecc0e665a76d7ac26e60866129e3e5
clams2tz <claudia.amstutz@outlook.com>
2024-10-09 16:37:16 +0200

    normalize lines to LF

--- src/components/ButtonDetails.vue
+++ src/components/ButtonDetails.vue
@@ -1,37 +1,34 @@
 <template>
-    <div>
-        <button @click="emitDetails">{{ label }}</button>
-    </div>
+  <div>
+    <button @click="emitDetails">{{ label }}</button>
+  </div>
 </template>
 
 <script>
-    export default {
-        props:{
-            label: {
-                type: String,
-                default: 'Show details'
-            },
-            color: {
-                type: String,
-                default: 'blue'
-            }
-        },
-        methods:{
-            emitDetails(){
-                this.$emit('show-details');
-            }
-        }
-    }
+export default {
+  props: {
+    label: {
+      type: String,
+      default: "Show details",
+    },
+    color: {
+      type: String,
+      default: "blue",npm i
+    },
+  },
+  methods: {
+    emitDetails() {
+      this.$emit("show-details");
+    },
+  },
+};
 </script>
 
 <style scoped>
-
-
 button {
-    background-color: v-bind(color);
-    border: none;
-    border-radius: 8px;
-    cursor: pointer;
+  background-color: v-bind(color);
+  border: none;
+  border-radius: 8px;
+  cursor: pointer;
 }
-
 </style>
