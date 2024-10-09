21f426aaf4f1cc41a8a7a3b63986d9315b9e3eef
ClementYanez <yanez.clement@live.fr>
2024-10-09 15:14:50 +0200

    mutation removeBook

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,14 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    listeBook: []
   },
   getters: {
   },
   mutations: {
+    removeBook(state, id){
+      state.listeBook = state.listeBook.filter((book) => id !== book.id)
+    }
   },
   actions: {
   },
